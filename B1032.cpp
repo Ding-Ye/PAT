719ba7df209b8721b88b9a0ee26b5b997c9584a1
yeding1992 <1092780440@qq.com>
2017-08-13 14:39:46 +0800

    1032

--- B1032.cpp
+++ B1032.cpp
@@ -8,11 +8,11 @@ int main()
 	int n, schID, score;
 	scanf("%d", &n);//总的参赛人数
 	for(int i = 0; i < n; i++){
-		scanf("%d%d", &schId, &score);
+		scanf("%d%d", &schID, &score);
 		school[schID] += score;
 	}
 	
-	int k = , MAX = -1;
+	int k = 1, MAX = -1;
 	for(int i = 1; i <=n; i ++){
 		if(school[i] > MAX){
 			MAX = school[i];
