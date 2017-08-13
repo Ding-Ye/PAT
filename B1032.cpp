63c58cecda22f1f288a9e591340f60002ae1a962
yeding1992 <1092780440@qq.com>
2017-08-13 13:13:31 +0800

    1032

---
+++ B1032.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+
+const int maxn = 100010;
+int school[maxn] = {0}; //记录每个学校的总分
+
+int main()
+{
+	int n, schID, score;
+	scanf("%d", &n);//总的参赛人数
+	for(int i = 0; i < n; i++){
+		scanf("%d%d", &schId, &score);
+		school[schID] += score;
+	}
+	
+	int k = , MAX = -1;
+	for(int i = 1; i <=n; i ++){
+		if(school[i] > MAX){
+			MAX = school[i];
+			k = i;
+		}
+	}
+	printf("%d %d\n", k, MAX);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
