35a935dd593a7c9a5146e6dd304074b59f446615
yeding1992 <1092780440@qq.com>
2017-08-14 21:26:17 +0800

    1011vs版本

---
+++ B1011/B1011/B1011/B1011.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+int main()
+{
+	int T, tcase = 1;
+	scanf("%d", &T);
+	while(T--){
+		long long a, b, c;
+		scanf("%lld%lld%lld", &a, &b, &c);
+		if(a+b > c){
+			printf("Case #%d: ture\n", tcase++);
+		}
+		else{
+			printf("Case #%d: false\n", tcase++);
+		}
+	}
+	return 0;
+}
