233e418a798188ef684db627803b0186ce44679e
yeding1992 <1092780440@qq.com>
2017-08-22 10:31:58 +0800

    1030

--- B1030.cpp
+++ B1030.cpp
@@ -1,3 +1,4 @@
+//题目理解出错
 #include <cstdio>
 #include <algorithm>
 using namespace std;
@@ -20,3 +21,141 @@ int main()
 	printf("%d", cout);
 	return 0;
 }
+
+
+//方法2
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+const int maxn = 100010;
+int n, p, a[maxn];
+
+int binarySerach(int i , long long x){
+	if(a[n - 1] <= x)
+		return n;
+	int l = i +1; 
+	int r = n - 1; 
+	int mid;
+	while(l < r){
+		mid = (1+r)/2;
+		if(a[mid] <= x){
+			l = mid + 1;
+		}
+		else{
+			r = mid;
+		}
+	}
+	return l;
+}
+
+int main()
+{
+	scanf("%d%d", &n, &p);
+	for(int i = 0; i < n; i++){
+		scanf("%d", &a[i]);
+	}
+	sort(a, a+n);
+	int ans = 1;
+	for(int i = 0; i < n; i++){
+		int j = binarySerach(i, (long long)a[i]*p);
+		ans = max(ans, j-i);
+	}
+	printf("%d\n", ans);
+	return 0;
+}
+
+//方法三
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+const int maxn = 100010;
+int n, p, a[maxn];
+int main()
+{
+	scanf("%d%d", &n, &p);
+	for(int i = 0; i < n; i++){
+		scanf("%d", &a[i]);
+	}
+	sort(a, a+n);
+	int ans = 1;
+	for(int i = 0; i < n; i++){
+		int j = upper_bound(a+i+1, a+n, (long long)a[i]*p) - a;
+		ans = max(ans, j-i);
+	}
+	printf("%d\n", ans);
+	return 0;
+}
+
+//方法三
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+const int maxn = 100010;
+int n, p, a[maxn];
+int main()
+{
+	scanf("%d%d", &n, &p);
+	for(int i = 0; i < n; i++){
+		scanf("%d", &a[i]);
+	}
+	sort(a, a+n);
+	int ans = 1;
+	int i = 0;
+	int j = n-1;
+    while(i < j){
+		if(a[j]/a[i] > p){
+			j--
+		}
+		ans = max(ans, j-i);
+	}
+	
+	printf("%d\n", ans);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
