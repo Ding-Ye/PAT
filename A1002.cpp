c60137ea59df081115471c4cc301dda34d9cfebf
yeding1992 <1092780440@qq.com>
2017-08-27 14:03:20 +0800

    1002

---
+++ A1002.cpp
@@ -0,0 +1,22 @@
+#incldue <cstdio>
+const int maxn = 1000;
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int a[maxn], b[maxn];
+	float a1[maxn], b1[maxn];
+	for(int i = 0; i < n; i++){
+		scanf("%d %f", &a[i], &a1[i]);0
+	}
+	
+	int m;
+	scanf("%d", &m);
+	int c[maxn], d[maxn];
+	float c1[maxn], d1[maxn];
+	for(int i = 0; i < m; i++){
+		scanf("%d %f", &c1[i], &d1[i]);
+	}
+	int count = 0;
+	return 0;	
+}
