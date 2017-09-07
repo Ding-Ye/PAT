d624bde4ede1c8f25fe22dedc3d27ed8d295cf81
yeding1992 <1092780440@qq.com>
2017-09-07 15:00:19 +0800

    1100

--- A1100/A1100/A1100/A1100.cpp
+++ A1100/A1100/A1100/A1100.cpp
@@ -1,30 +1,18 @@
-#include <iostream>
-#include <map>
-#include <string>
-#include <cctype>
-using namespace std;
-int main() {
-    string s, t;
-    getline(cin, s);
-    map<string, int> m;
-    for(int i = 0; i < s.length(); i++) {
-        if(isalnum(s[i])) {
-            s[i] = tolower(s[i]);
-            t += s[i];
-        } else {
-            if(t.length())
-                m[t]++;
-            t = "";
-        }
-    }
-    if(t.length()) m[t]++;
-    int maxn = 0;
-    for(map<string, int>::iterator it = m.begin(); it != m.end(); it++) {
-        if(it->second > maxn) {
-            maxn = it->second;
-            t = it->first;
-        }
-    }
-    cout << t << " " << maxn;
-    return 0;
+#include<iostream> 
+#include<string> 
+using namespace std; 
+int main () 
+{ 
+	string str; 
+	cin >>str;
+    cout<<str<<endl; 
+	char c = getchar();
+	string str1;
+
+	getline(cin,str1); 
+	string str2;
+	getline(cin,str2); 
+	cout<<str1<<endl; 
+	cout<<str2<<endl; 
+	return 0;
 }
