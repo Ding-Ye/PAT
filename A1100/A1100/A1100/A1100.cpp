0584f71521d8a2ea2b246d7a2b8b609529f65ea2
yeding1992 <1092780440@qq.com>
2017-09-09 20:00:03 +0800

    1100

--- A1100/A1100/A1100/A1100.cpp
+++ A1100/A1100/A1100/A1100.cpp
@@ -1,18 +1,39 @@
-#include<iostream> 
-#include<string> 
-using namespace std; 
-int main () 
-{ 
-	string str; 
-	cin >>str;
-    cout<<str<<endl; 
-	char c = getchar();
-	string str1;
-
-	getline(cin,str1); 
-	string str2;
-	getline(cin,str2); 
-	cout<<str1<<endl; 
-	cout<<str2<<endl; 
+#include<cstdio>
+#include<stack>
+using namespace std;
+const int maxn = 1010;
+int arr[maxn];
+stack<int> st;
+int main()
+{
+	int m, n, T;
+	scanf("%d %d %d", &m, &n, &T);
+	while(T--){
+		while(!st.empty()){//Çå¿ÕÕ»
+			st.pop();
+		}
+		for(int i = 1; i <= n; i++){
+			scanf("%d", &arr[i]);
+		}
+		int current = 1;
+		bool flag = true;
+		for(int i = 1; i <= n; i++){
+			st.push(i);
+			if(st.size() > m){
+				flag = false;
+				break;
+			}
+			while(!st.empty() && st.top() == arr[current]){
+				st.pop();
+				current++;
+			}
+		}
+		if(st.empty() == true && flag == true){
+			printf("Yes\n");
+		}
+		else{
+			printf("No\n");
+		}
+	}
 	return 0;
 }
