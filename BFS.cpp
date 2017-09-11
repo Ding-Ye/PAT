f8837c628b30c06483def2ce28772549b9b66e35
Ding-Ye <1092780440@qq.com>
2017-09-11 21:27:04 +0800

    BFS

---
+++ BFS.cpp
@@ -0,0 +1,24 @@
+void BFS(int s){
+	queue<int> q;
+	q.push(s);
+	while(!q.empty()){
+		取出队首元素top；
+		访问队首元素top；
+		将队首元素出队；
+		将top的下一层结点中未曾入队的结点全部入队，并设置为已入队
+	}
+}
+
+void BFS(int x, int y){
+	queue<node> Q;
+	Node.x = x, Node.y = y;
+	Q.push(Node);
+	inq[x][y] = true;
+	while(!Q.empty()){
+		node top = Q.front();
+		Q.pop();
+		for(int i = 0; i < 4; i++){
+			int newX = top.x + X[i]
+		}
+	}
+}
