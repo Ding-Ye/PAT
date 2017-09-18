a217e6104a3b2baebc612cb02b15f42e884a0a18
Ding-Ye <1092780440@qq.com>
2017-09-18 23:49:09 +0800

    二叉查找树

--- 二叉查找树.cpp
+++ 二叉查找树.cpp
@@ -21,3 +21,90 @@
 	}
 	
 	2.2插入
+	void insert(node* &root, int x){
+		if(root == NULL){
+			root = newNod.e(x);
+			return;
+		}
+		if(x == root->data){
+			return;
+		}
+		else if(x < root->data){
+			insert(root->lchild, x);
+		}
+		else{
+			insert(root->rchild, x);
+		}
+	}
+	
+	2.3建立
+	node* Create(int data[], int n){
+		node* root = NULL;
+		for(int i = 0; i < n; i++){
+			insert(root, data[i]);
+		}
+		
+		return root;
+	}
+	
+	2.4删除
+		2.4.1后继
+		node* findMax(node* root){
+			while(root->rchild != NULL){
+				root = root -> rchild;
+			}
+			return root;
+		}
+		
+		2.4.2前驱
+		node* findMix(node* root){
+			while(root->lchild != NULL){
+				root = root -> lchild;
+			}
+			return root;
+		}
+		
+		2.4.3删除
+		void deleteNode(node* &root, int x){
+			if(root == NULL)
+				return;
+			if(root->data == x){
+				if(root->lchild == NULL && root->rchild == NULL){
+					root = NULL;
+				}
+				else if(root->lchild != NULL){
+					node* pre = findMax(root->lchild);
+					root->data = pre->data;
+					deleteNode(root->lchild, pre->data);//????
+				}
+				else{
+					node* next = findMin(root->rchild);
+					root->data = next->data;
+					deleteNode(root->rchild, next->data);//????
+				}
+			}
+			else if(root->data > x){
+				deleteNode(root->lchild, x);
+			}
+			else{
+				deleteNode(root->rchild, x);
+			}
+		}
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
