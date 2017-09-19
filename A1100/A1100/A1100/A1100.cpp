e217a16e0aa5326060da7878ec2fcdfddaef35b5
Ding-Ye <1092780440@qq.com>
2017-09-19 21:09:45 +0800

    1100

--- A1100/A1100/A1100/A1100.cpp
+++ A1100/A1100/A1100/A1100.cpp
@@ -1,60 +1,56 @@
 #include <cstdio>
-#include <vector>
+#include <queue>// 用BFS做层遍历
 #include <algorithm>
 using namespace std;
+const int maxn = 110;
 
-const int MAXN = 110;
 struct node{
-	int weight;
-	vector<int> child;//指针域
-}Node[MAXN];
+	int data;
+	int lchild, rchild;
+}Node[maxn];
 
-bool cmp(int a, int b){
-	return Node[a].weight > Node[b].weight;
-}
-
-int n, m, S;
-int path[MAXN]; //记录路径
-
-void DFS(int index, int numNode, int sum){
-	if(sum > S)
-		return;
-	if(sum == S){
-		if(Node[index].child.size() != 0)
-			return;
-		for(int i = 0; i < numNode; i++){
-			printf("%d", Node[path[i]].weight);
-			if(i < numNode - 1)
-				printf(" ");
-			else
-				printf("\n");
-		}
+int n, in[maxn], num = 0;
+void inOrder(int root){
+	if(-1 == root){
 		return;
 	}
-	
-	for(int i = 0; i < Node[index].child.size(); i++){
-		int child = Node[index].child[i];
-		path[numNode] = child; // ??
-		DFS(child, numNode + 1, sum + Node[child].weight);
+	inOrder(Node[root].lchild);
+	Node[root].data = in[num++];
+	inOrder(Node[root].rchild);
+}
+
+void BFS(int root){
+	queue<int> q;
+	q.push(root);
+	num = 0;
+	while(!q.empty()){
+		int now = q.front();
+		q.pop();
+		printf("%d", Node[now].data);
+		num++;
+		if(num < n)
+			printf(" ");
+		if(Node[now].lchild != -1)
+			q.push(Node[now].lchild);
+		if(Node[now].rchild != -1)
+			q.push(Node[now].rchild);
 	}
 }
 
 int main(){
-	scanf("%d %d %d", &n, &m, &S);
+	int lchild, rchild;
+	scanf("%d", &n);
 	for(int i = 0; i < n; i++){
-		scanf("%d", &Node[i].weight);
+		scanf("%d %d", &lchild, &rchild);
+		Node[i].lchild = lchild;
+		Node[i].rchild = rchild;
 	}
-	int id, k, child;
-	for(int i = 0; i < m; i++){
-		scanf("%d %d", &id, &k);
-		for(int j = 0; j < k; j++){
-			scanf("%d", &child);
-			Node[id].child.push_back(child);
-		}
-		sort(Node[id].child.begin(), Node[id].child.end(), cmp);
+	for(int i = 0; i < n; i++){
+		scanf("%d", &in[i]);
 	}
-	path[0] = 0;
-	DFS(0, 1, Node[0].weight);
+	sort(in, in + n);
+	inOrder(0);
+	BFS(0);
 	return 0;
 }
 
