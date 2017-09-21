34854a453ca53d209615857309e5d0839bd3c91c
Ding-Ye <1092780440@qq.com>
2017-09-21 20:57:14 +0800

    好朋友

--- 好朋友.cpp
+++ 好朋友.cpp
@@ -1,8 +1,9 @@
 #include <cstdio>
 const int N = 110;
-int father[N];
-bool isRoot[N];
-int findFather(int x){
+int father[N]; //存放父节点
+bool isRoot[N]; //记录每个节点是否作为某个集合的根节点
+
+int findFather(int x){ //查找x所在集合的根节点
 	int a = x;
 	while(x != father[x]){
 		x = father[x];
@@ -17,7 +18,7 @@ int findFather(int x){
 	return x;
 }
 
-void Union(int a, int b){
+void Union(int a, int b){//合并a和b所在的集合
 	int faA = findFather(a);
 	int faB = findFather(b);
 	if(faA != faB){
@@ -25,19 +26,20 @@ void Union(int a, int b){
 	}
 }
 
-void init(int n){
+void init(int n){//初始化father[i]为i，且flag[i]为false
 	for(int i = 1; i <= n; i++){
-		father[i] = i;
+		father[i] = i;//每个节点都是自己根节点
 		isRoot[i] = false;
 	}
 }
+
 int main(){
 	int n, m, a, b;
 	scanf("%d %d", &n, &m);
 	init(n);
 	for(int i = 0; i < m; i++){
-		scanf("%d %d", &a, &b);
-		Union(a, b);
+		scanf("%d %d", &a, &b);//输入两个好朋友的关系
+		Union(a, b);//合并a和b所在的集合
 	}
 	for(int i = 1; i <= n; i++){
 		isRoot[findFather(i)] = true;
@@ -47,27 +49,6 @@ int main(){
 		ans += isRoot[i];
 	}
 	printf("%d\n", ans);
-	return 0
+	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
