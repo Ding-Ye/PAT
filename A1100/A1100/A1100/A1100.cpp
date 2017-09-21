31d90a1e85f46b0a211f1f1fcb18b662152e9d1b
Ding-Ye <1092780440@qq.com>
2017-09-21 09:02:32 +0800

    1100

--- A1100/A1100/A1100/A1100.cpp
+++ A1100/A1100/A1100/A1100.cpp
@@ -1,56 +1,101 @@
 #include <cstdio>
-#include <queue>// ÓÃBFS×ö²ã±éÀú
 #include <algorithm>
 using namespace std;
-const int maxn = 110;
-
 struct node{
-	int data;
-	int lchild, rchild;
-}Node[maxn];
+	int v, height;
+	node* lchild, *rchild;
+}*root;
 
-int n, in[maxn], num = 0;
-void inOrder(int root){
-	if(-1 == root){
-		return;
-	}
-	inOrder(Node[root].lchild);
-	Node[root].data = in[num++];
-	inOrder(Node[root].rchild);
+node* newNode(int v){
+	node* Node = new node;
+	Node->v = v;
+	Node->height = 1;
+	Node->lchild = Node->rchild = NULL;
+	
+	return Node;
 }
 
-void BFS(int root){
-	queue<int> q;
-	q.push(root);
-	num = 0;
-	while(!q.empty()){
-		int now = q.front();
-		q.pop();
-		printf("%d", Node[now].data);
-		num++;
-		if(num < n)
-			printf(" ");
-		if(Node[now].lchild != -1)
-			q.push(Node[now].lchild);
-		if(Node[now].rchild != -1)
-			q.push(Node[now].rchild);
+int getHeight(node* root){
+	if(root == NULL)
+		return 0;
+	return root->height;
+}
+
+void updateHeight(node* root){
+	root->height = max(getHeight(root->lchild), getHeight(root->rchild)) + 1;
+}
+
+int getBalanceFactor(node* root){
+	return getHeight(root->lchild) - getHeight(root->rchild);
+}
+
+void L(node* &root){
+	node* temp = root->rchild;
+	root->rchild = temp->lchild;
+	temp->lchild = root;
+	updateHeight(root);
+	updateHeight(temp);
+	root = temp;
+}
+
+void R(node* &root){
+	node* temp = root->lchild;
+	root->lchild = temp->rchild;
+	temp->rchild = root;
+	updateHeight(root);
+	updateHeight(temp);
+	root = temp;
+}
+
+void insert(node* &root, int v){
+	if(root == NULL){
+		root = newNode(v);
+		return;
 	}
+	if(v < root->v){
+		insert(root->lchild, v);
+		updateHeight(root);
+		if(getBalanceFactor(root) == 2){
+			if(getBalanceFactor(root->lchild) == 1){
+				R(root);
+			}
+			else if(getBalanceFactor(root->lchild) == -1){
+				L(root->lchild);
+				R(root);
+			}
+		}
+	}
+	else{
+		insert(root->rchild, v);
+		updateHeight(root);
+		if(getBalanceFactor(root) == -2){
+			if(getBalanceFactor(root->rchild) == -1){
+				L(root);
+			}
+			else if(getBalanceFactor(root->rchild) == 1){
+				R(root->rchild);
+				L(root);
+			}
+		}
+	}
+}
+
+node* Create(int data[], int n){
+	node* root = NULL;
+	for(int i = 0; i < n; i++){
+		insert(root, data[i]);
+	}
+	return root;
 }
 
 int main(){
-	int lchild, rchild;
+	int n, v;
 	scanf("%d", &n);
 	for(int i = 0; i < n; i++){
-		scanf("%d %d", &lchild, &rchild);
-		Node[i].lchild = lchild;
-		Node[i].rchild = rchild;
+		scanf("%d", &v);
+		insert(root, v);
 	}
-	for(int i = 0; i < n; i++){
-		scanf("%d", &in[i]);
-	}
-	sort(in, in + n);
-	inOrder(0);
-	BFS(0);
+	printf("%d\n", root->v);
 	return 0;
 }
 
