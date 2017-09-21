66ab227f37094d145a1496579ae75a040d2a3993
Ding-Ye <1092780440@qq.com>
2017-09-21 20:57:48 +0800

    1100

--- A1100/A1100/A1100/A1100.cpp
+++ A1100/A1100/A1100/A1100.cpp
@@ -1,101 +1,74 @@
 #include <cstdio>
 #include <algorithm>
 using namespace std;
-struct node{
-	int v, height;
-	node* lchild, *rchild;
-}*root;
+const int N = 1010;
+int father[N];
+bool isRoot[N];
+int course[N] = {0};
 
-node* newNode(int v){
-	node* Node = new node;
-	Node->v = v;
-	Node->height = 1;
-	Node->lchild = Node->rchild = NULL;
+int findFather(int x){
+	int a = x;
+	while(x != father[x]){
+		x = father[x];
+	}
 	
-	return Node;
-}
-
-int getHeight(node* root){
-	if(root == NULL)
-		return 0;
-	return root->height;
-}
-
-void updateHeight(node* root){
-	root->height = max(getHeight(root->lchild), getHeight(root->rchild)) + 1;
-}
-
-int getBalanceFactor(node* root){
-	return getHeight(root->lchild) - getHeight(root->rchild);
-}
-
-void L(node* &root){
-	node* temp = root->rchild;
-	root->rchild = temp->lchild;
-	temp->lchild = root;
-	updateHeight(root);
-	updateHeight(temp);
-	root = temp;
-}
-
-void R(node* &root){
-	node* temp = root->lchild;
-	root->lchild = temp->rchild;
-	temp->rchild = root;
-	updateHeight(root);
-	updateHeight(temp);
-	root = temp;
-}
-
-void insert(node* &root, int v){
-	if(root == NULL){
-		root = newNode(v);
-		return;
+	while(a != father[a]){
+		int z = a;
+		a = father[a];
+		father[z] = x;
 	}
-	if(v < root->v){
-		insert(root->lchild, v);
-		updateHeight(root);
-		if(getBalanceFactor(root) == 2){
-			if(getBalanceFactor(root->lchild) == 1){
-				R(root);
-			}
-			else if(getBalanceFactor(root->lchild) == -1){
-				L(root->lchild);
-				R(root);
-			}
-		}
-	}
-	else{
-		insert(root->rchild, v);
-		updateHeight(root);
-		if(getBalanceFactor(root) == -2){
-			if(getBalanceFactor(root->rchild) == -1){
-				L(root);
-			}
-			else if(getBalanceFactor(root->rchild) == 1){
-				R(root->rchild);
-				L(root);
-			}
-		}
+	return x;
+}
+
+void Uion(int a, int b){
+	int faA = findFather(a);
+	int faB = findFather(b);
+	if(faA != faB){
+		father[faA] = faB;
 	}
 }
 
-node* Create(int data[], int n){
-	node* root = NULL;
-	for(int i = 0; i < n; i++){
-		insert(root, data[i]);
+void init(int n){
+	for(int i = 1; i <= n; i++){
+		father[i] = i;
+		isRoot[i] = false;
 	}
-	return root;
+}
+
+bool cmp(int a, int b){
+	return a > b;
 }
 
 int main(){
-	int n, v;
+	int n, k , h;
 	scanf("%d", &n);
-	for(int i = 0; i < n; i++){
-		scanf("%d", &v);
-		insert(root, v);
+	init(n);
+	for(int i = 1; i <= n; i++){
+		scanf("%d:", &k);
+		for(int j = 0; j < k; j++){
+			scanf("%d", &h);
+			if(course[h] == 0){
+				course[h] = i;
+			}
+			Uion(i, findFather(course[h]));
+		}
 	}
-	printf("%d\n", root->v);
+	
+	for(int i = 1; i <= n; i++){
+		isRoot[findFather(i)]++;
+	}
+	int ans = 0;
+	for(int i = 1; i <= n; i++){
+		if(isRoot[i] != 0){
+			ans++;
+		}
+	}
+	printf("%d\n", ans);
+	sort(isRoot + 1, isRoot + n + 1, cmp);
+	for(int i = 1; i <= ans; i++){
+		printf("%d", course[i]);
+		if(i < ans)
+			printf(" ");
+	}
 	return 0;
 }
-
