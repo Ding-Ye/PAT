fc57d4eebfc5738679af4ddfc216e90f4b07b400
Ding-Ye <1092780440@qq.com>
2017-09-22 19:28:18 +0800

    1100

--- A1100/A1100/A1100/A1100.cpp
+++ A1100/A1100/A1100/A1100.cpp
@@ -1,74 +1,107 @@
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include <cassert>
+
 using namespace std;
-const int N = 1010;
-int father[N];
-bool isRoot[N];
-int course[N] = {0};
 
-int findFather(int x){
-	int a = x;
-	while(x != father[x]){
-		x = father[x];
-	}
-	
-	while(a != father[a]){
-		int z = a;
-		a = father[a];
-		father[z] = x;
-	}
-	return x;
-}
-
-void Uion(int a, int b){
-	int faA = findFather(a);
-	int faB = findFather(b);
-	if(faA != faB){
-		father[faA] = faB;
-	}
-}
-
-void init(int n){
-	for(int i = 1; i <= n; i++){
-		father[i] = i;
-		isRoot[i] = false;
-	}
-}
-
-bool cmp(int a, int b){
-	return a > b;
-}
+template<typename Item>
+class MaxHeap{
+	private:
+		Item* data;
+		int count;
+		int capacity; 
+		
+		void shiftUp(int k){
+			while(data[k/2] < data[k] && k > 1){
+				swap(data[k/2], data[k]);
+				k /= 2;
+			}
+		}
+		
+		void shiftDown(int k){
+			while(2*k <= count){
+				int j = 2*k;
+				if(j + 1 <= count && data[j + 1] > data[j]){
+					 j = j + 1;
+				}
+				
+				if(data[k] >= data[j]){
+					break;
+				}
+				
+				swap(data[k], data[j]);
+				k = j;
+			}
+		}
+		
+	public:
+		MaxHeap(int capacity){
+			data = new Item[capacity + 1];
+			count = 0;
+			this->capacity = capacity;
+		}
+		
+		~MaxHeap(){
+			delete[] data;
+		}
+		
+		int size(){
+			return count;
+		}
+		
+		bool isEmpty(){
+			return count == 0;
+		}
+		
+		void insert(Item item){
+			assert(count + 1 <= capacity);
+			data[count + 1] = item;
+			count ++;
+			shiftUp(count);
+		}
+		
+		Item extractMax(){
+			assert(count > 0);
+			Item ret = data[1];
+			
+			swap(data[1], data[count]);
+			count--;
+			shiftDown(1);
+			
+			return ret;
+		}
+};
 
 int main(){
-	int n, k , h;
-	scanf("%d", &n);
-	init(n);
-	for(int i = 1; i <= n; i++){
-		scanf("%d:", &k);
-		for(int j = 0; j < k; j++){
-			scanf("%d", &h);
-			if(course[h] == 0){
-				course[h] = i;
-			}
-			Uion(i, findFather(course[h]));
-		}
+	MaxHeap<int> maxheap = MaxHeap<int>(100);
+	
+	srand(time(NULL));
+	for(int i = 0; i < 15; i++){
+		maxheap.insert(rand() % 100);
 	}
 	
-	for(int i = 1; i <= n; i++){
-		isRoot[findFather(i)]++;
+	while(!maxheap.isEmpty()){
+		cout << maxheap.extractMax() <<" ";
 	}
-	int ans = 0;
-	for(int i = 1; i <= n; i++){
-		if(isRoot[i] != 0){
-			ans++;
-		}
-	}
-	printf("%d\n", ans);
-	sort(isRoot + 1, isRoot + n + 1, cmp);
-	for(int i = 1; i <= ans; i++){
-		printf("%d", course[i]);
-		if(i < ans)
-			printf(" ");
-	}
+	cout << endl;
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
